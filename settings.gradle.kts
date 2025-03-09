8493859f4026a68908aa7ce7e6b957a70f8ac6d5
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-03-09 23:43:31 +0100

    setup Java toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ rootProject.name = "minimal-spigot-plugin"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
